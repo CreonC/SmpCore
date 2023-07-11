9a5fad31960b360d76e496e5fcccddb832c5c265
CreonC <creon080@gmail.com>
2023-07-11 23:04:15 +0900

    Added todo list and command

--- src/main/java/me/creonc/smpcore/SmpCore.java
+++ src/main/java/me/creonc/smpcore/SmpCore.java
@@ -1,18 +1,38 @@
 package me.creonc.smpcore;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
-public final class SmpCore extends JavaPlugin {
+import java.util.Objects;
+
+public final class SmpCore extends JavaPlugin { //Fun Fact: This is very poor written.
+
+
+    public boolean onCommand(@NotNull CommandSender sender, Command command, @NotNull String label, String[] args) {
+        // Handle the command
+        if (command.getName().equals("SelectClass")) {
+            sender.sendMessage("Hello, world!");
+        }
+        return true;
+    }
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         Bukkit.getLogger().info("Starting Plugin");
+        Objects.requireNonNull(getCommand("SelectClass")).setExecutor(this);
+        //TODO: Register command, set executor and load abu
     }
+
+
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        Bukkit.getLogger().info("Stopping Plugin");
+        // TODO: Shutdown logic
     }
 }
