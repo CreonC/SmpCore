65439a48eea7bdd24ab7880cc725e94691dc3381
CreonC <69414582+CreonC@users.noreply.github.com>
2023-07-19 20:29:39 +0800

    selectclass confirm and cancel

--- src/main/java/me/creonc/smpcore/commands/SelectClass.java
+++ src/main/java/me/creonc/smpcore/commands/SelectClass.java
@@ -6,35 +6,78 @@ import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
 public class SelectClass implements CommandExecutor {
-    @Override
+    private String selectedClass;
+
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if (command.getName().equals("selectclass")) {
-            if (args.length != 1) {
-                sender.sendMessage("Usage: /selectclass <class>");
-                sender.sendMessage("Available classes: berserker, alchemist, healer");
+        if (!command.getName().equals("selectclass")) {
+            return false;
+        }
+
+        if (args.length == 0) {
+            sender.sendMessage("Usage: /selectclass <class>");
+            sender.sendMessage("Available classes: berserker, alchemist, healer");
+            return true;
+        }
+
+        if (args[0].equalsIgnoreCase("confirm")) {
+            if (selectedClass == null) {
+                sender.sendMessage("No class selected. Use the /selectclass command to select a class.");
                 return true;
             }
 
-            String className = args[0].toLowerCase();
-            if (!(className.equals("berserker") || className.equals("alchemist") || className.equals("healer"))) {
-                sender.sendMessage("Invalid class name. Available classes: berserker, alchemist, healer");
+            // Apply the selected class to the player
+            // ...
+
+            sender.sendMessage("Selected class confirmed: " + selectedClass);
+            selectedClass = null;
+
+            return true;
+        } else if (args[0].equalsIgnoreCase("cancel")) {
+            if (selectedClass == null) {
+                sender.sendMessage("No class selected. Use the /selectclass command to select a class.");
                 return true;
             }
 
-            sender.sendMessage("Selected class: " + className);
-
-            // Print abilities for the selected class
-            switch (className) {
-                case "berserker" -> sender.sendMessage("Abilities: Swing (axes), 10% more damage to players, Thump");
-                case "alchemist" -> sender.sendMessage("Abilities: Potion Bag, God Potion");
-                case "healer" -> sender.sendMessage("Abilities: +50% health regen, Spray of Life");
-            }
-
-            // Ask player to confirm their selection
-            sender.sendMessage("Confirm selection? Type /confirm to confirm or /cancel to cancel.");
+            sender.sendMessage("Selected class canceled: " + selectedClass);
+            selectedClass = null;
 
             return true;
         }
-        return false;
+
+        if (args.length != 1) {
+            sender.sendMessage("Usage: /selectclass <class>");
+            sender.sendMessage("Available classes: berserker, alchemist, healer");
+            return true;
+        }
+
+        String className = switch (args[0].toLowerCase()) {
+            case "berserker" -> "Berserker";
+            case "alchemist" -> "Alchemist";
+            case "healer" -> "Healer";
+            default -> null;
+        };
+
+        if (className == null) {
+            sender.sendMessage("Invalid class name. Available classes: berserker, alchemist, healer");
+            return true;
+        }
+
+        sender.sendMessage("Selected class: " + className);
+
+        String abilities = switch (className.toLowerCase()) {
+            case "berserker" -> "Abilities: Swing (axes), 10% more damage to players, Thump";
+            case "alchemist" -> "Abilities: Potion Bag, God Potion";
+            case "healer" -> "Abilities: +50% health regen, Spray of Life";
+            default -> null;
+        };
+
+        if (abilities != null) {
+            sender.sendMessage(abilities);
+        }
+
+        sender.sendMessage("Confirm selection? Type /selectclass confirm to confirm or /selectclass cancel to cancel.");
+        selectedClass = className;
+
+        return true;
     }
 }
