cf6a7b37a023cc47185fb95b8ee4044ab34bf967
CreonC <69414582+CreonC@users.noreply.github.com>
2023-07-19 19:19:11 +0800

    Fix NullPointer #4

--- src/main/java/me/creonc/smpcore/SmpCore.java
+++ src/main/java/me/creonc/smpcore/SmpCore.java
@@ -5,15 +5,14 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Objects;
-
-public final class SmpCore extends JavaPlugin { //Fun Fact: This is very poorly written.
+public final class SmpCore extends JavaPlugin {
 
 
     public boolean onCommand( CommandSender sender, Command command, String label, String[] args) {
         // Handle the command
         if (command.getName().equals("selectclass")) {
             sender.sendMessage("Hello, world!"); //How to fix deprecated
+            return true;
         }
         return true;
     }
@@ -22,7 +21,8 @@ public final class SmpCore extends JavaPlugin { //Fun Fact: This is very poorly 
     public void onEnable() {
         // Plugin startup logic
         Bukkit.getLogger().info("Starting Plugin");
-        this.getCommand("selectclass").setExecutor(this); //pls
+        this.getCommand("selectclass").setExecutor(this::onCommand); //pls
+
         //TODO: Register command, set executor and load abu
     }
 
